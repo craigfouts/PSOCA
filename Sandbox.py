bad4152e5856e4172ef27b20ace5c0c47063e155
Craig Fouts <foutscw@gmail.com>
2021-05-19 23:40:28 -0400

    Implemented basic CA rule for converting a 4x4 square into a 2x8 rectangle

--- Sandbox.py
+++ Sandbox.py
@@ -8,7 +8,6 @@ class Window(pyglet.window.Window):
     def __init__(self, width, height, CellularAutomata, *args, **kwargs):
         super().__init__(width, height, *args, **kwargs)
         self.ca = CellularAutomata(width, height, 50, 0.5)
-        pyglet.clock.schedule_interval(self.update, 1.0 / 24.0)
 
     def update(self, dt):
         self.ca.run_rules()
@@ -17,6 +16,10 @@ class Window(pyglet.window.Window):
         self.clear()
         self.ca.draw()
 
+    def on_key_press(self, symbol, modifier):
+        if symbol == pyglet.window.key.SPACE:
+            pyglet.clock.schedule_interval(self.update, 1.0 / 4.0)
+            
 
 if __name__ == '__main__':
     window = Window(600, 600, SelfOrganizingCA)
